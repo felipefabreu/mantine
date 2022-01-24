25ada90b68e4a49fe140fd338f13fd584a1744e7
Felipe <felipefabreu@gmail.com>
2022-01-24 11:04:59 -0300

    testando alteração de tema

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { SunIcon, MoonIcon } from '@modulz/radix-icons';
 
 import { 
   MantineProvider, 
@@ -6,42 +7,77 @@ import {
   AppShell, 
   Navbar, 
   Header,
-  Text 
+  Text, 
+  Title,
+  ColorScheme,
+  useMantineColorScheme,
+  ColorSchemeProvider,
+  ActionIcon,
+  
 } from '@mantine/core';
+import { useHotkeys, useLocalStorageValue } from '@mantine/hooks';
 
 const App: React.FC = () => {
+
+  const [colorScheme, setColorScheme] = useLocalStorageValue<ColorScheme>({
+    key: 'mantine-color-scheme',
+    defaultValue: 'light',
+  });
+
+  const toggleColorScheme = (value?: ColorScheme) =>
+  setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+
+  useHotkeys([['mod+J', () => toggleColorScheme()]]);
+
   return (
-    <AppShell
-      padding="md"
-      navbar={
-        <Navbar height={600} padding="xs" width={{ base: 300 }}>
-        <Navbar.Section>
-          <Text>
-            Dashboards
-          </Text>
-        </Navbar.Section>
-        <Navbar.Section>
-          <Text>  
-            KPIs
-          </Text>
-        </Navbar.Section>
-        <Navbar.Section>
-          <Text>
-            Carga de dados
-          </Text>
-        </Navbar.Section>
-      </Navbar>
-      }
-      
-      header={<Header height={60} padding="xs">
-        {/* Header content */}
-      </Header>}
-      styles={(theme) => ({
-        main: { backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0] },
-      })}
-    >
-      {/* Your application here */}
-    </AppShell>
+    <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+      <MantineProvider theme={{ colorScheme }}>
+        <AppShell
+          padding="md"
+          navbar={
+            <Navbar  padding="xs" width={{ base: 300 }}>
+            <Navbar.Section>
+              <Text color='blue' variant='link' component='a' href='#' >
+                Dashboards
+              </Text>
+            </Navbar.Section>
+            <Navbar.Section>
+              <Text>  
+                KPIs
+              </Text>
+            </Navbar.Section>
+            <Navbar.Section>
+              <Text>
+                Carga de dados
+              </Text>
+            </Navbar.Section>
+          </Navbar>
+          }
+          
+          header={<Header height={60} padding="md" >
+             
+             <ActionIcon
+              variant="outline"
+              color={colorScheme === 'dark' ? 'yellow' : 'blue'}
+              onClick={() => toggleColorScheme()}
+              title="Toggle color scheme"
+            >
+              {colorScheme === 'dark' ? (
+                <SunIcon style={{ width: 18, height: 18 }} />
+              ) : (
+                <MoonIcon style={{ width: 18, height: 18 }} />
+              )}
+            </ActionIcon>
+
+          </Header>}
+          styles={(theme) => ({
+            main: { backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0] },
+          })}
+        >
+          <Title>Aplicação</Title>
+        </AppShell>
+      </MantineProvider>
+    </ColorSchemeProvider>
   );
 
 }
@@ -49,12 +85,12 @@ const App: React.FC = () => {
 // Custom theme is applied to all components in App
 function WithProvider() {
   return (
-    <MantineProvider theme={{ colorScheme: 'dark' }}>
+    
       <App />
-    </MantineProvider>
+    
   );
 }
 
 
 
-export default WithProvider;
+export default App;
